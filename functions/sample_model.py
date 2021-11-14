5c2b2f56ab55c2fbf53fff139d1c07c574f0da40
Saksham Checker <checkersaksham@yahoo.com>
2021-11-14 22:35:23 +0530

    added Sample model

---
+++ functions/sample_model.py
@@ -0,0 +1,10 @@
+def sample_model(vocab_size):
+    model = Sequential()
+    model.add(Embedding(vocab_size, 512, batch_input_shape=(1, 1)))
+    for i in range(4):
+        model.add(LSTM(256, return_sequences=(i != 3), stateful=True))
+        model.add(Dropout(0.2))
+
+    model.add(Dense(vocab_size))
+    model.add(Activation('softmax'))
+    return model
