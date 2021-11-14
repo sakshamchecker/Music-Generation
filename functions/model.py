d286f18f37fd8acf1b08eebb5a242399c298715c
Saksham Checker <checkersaksham@yahoo.com>
2021-11-14 22:34:28 +0530

    added model

---
+++ functions/model.py
@@ -0,0 +1,10 @@
+def model(batch_size, seq_len, vocab_size):
+    model = Sequential()
+    model.add(Embedding(vocab_size, 512, batch_input_shape=(batch_size, seq_len)))
+    for i in range(4):
+        model.add(LSTM(256, return_sequences=True, stateful=True))
+        model.add(Dropout(0.2))
+
+    model.add(TimeDistributed(Dense(vocab_size))) 
+    model.add(Activation('softmax'))
+    return model
